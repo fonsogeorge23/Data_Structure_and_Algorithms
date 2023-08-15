aba27af77546346c631e50eaf080c0585a92f540
TLTTVMLT214\TLTUser <fonsogeorge@gmail.com>
2023-08-15 21:59:33 +0530

    First change commit

--- Arrays/DSA_Arrays/src/Easy/Easy_Client.java
+++ Arrays/DSA_Arrays/src/Easy/Easy_Client.java
@@ -1,2 +1,14 @@
-package Easy;public class Easy_Client {
+package Easy;
+
+import java.util.Arrays;
+
+public class Easy_Client {
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 3, 6, 7, 5};
+        int rotate = 2;
+        RotationGame r = new RotationGame();
+
+        r.RotationGame(array, rotate);
+        System.out.println(Arrays.toString(array));
+    }
 }
--- Arrays/DSA_Arrays/src/Easy/RotationGame.java
+++ Arrays/DSA_Arrays/src/Easy/RotationGame.java
@@ -1,2 +1,35 @@
-package Easy;public class RotationGame {
+/*
+Given an integer array A of size N and an integer B, you have to print the same array after rotating it B times towards the right.
+
+Constraint:
+1 <= N <= 10^5
+-10^5 <= A[i] <= 10^5
+
+Sample:
+Input : 4 1 2 3 4
+        2
+
+Output: 3 4 1 2
+*/
+package Easy;
+
+public class RotationGame {
+    int[] RotationGame(int[] array, int rotate){
+        int n = array.length;
+        reverse(array, 0, n-1);
+        reverse(array, 0, rotate-1);
+        reverse(array, rotate, n-1);
+        return array;
+    }
+
+    void reverse(int[] arr, int s, int e){
+        int n = arr.length;
+        while(s < e){
+            arr[s] += arr[e];
+            arr[e] = arr[s] - arr[e];
+            arr[s] = arr[s] - arr[e];
+            s++;
+            e--;
+        }
+    }
 }
